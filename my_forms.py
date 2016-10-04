cae37b26555ddeaa8bffec619799ca764020cb34
Michael-Jalloh <michaeljalloh19@gmail.com>
2016-10-04 11:01:59 +0000

    Updates

--- my_forms.py
+++ my_forms.py
@@ -1,4 +1,4 @@
-import flask_wtf as Form
+from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import Required, Email, Length, Regexp, EqualTo
 
